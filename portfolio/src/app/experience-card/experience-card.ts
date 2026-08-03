23a1cb12cd2aede69cbf4ef3c3118dc33a6974d4
rrsharpe <62400551+rrsharpe@users.noreply.github.com>
2026-08-03 16:20:57 -0400

    switch to dayjs

--- portfolio/src/app/experience-card/experience-card.ts
+++ portfolio/src/app/experience-card/experience-card.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/material/card';
 import { MatChipSet, MatChip } from '@angular/material/chips';
 import { JobDescription } from '../job-descriptions';
-import moment from 'moment';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-experience-card',
@@ -30,11 +30,11 @@ export class ExperienceCard {
   readonly dateString = computed(() => {
     const job = this.job();
     const endDate = job.endDate === 'Present' ? new Date(Date.now()) : job.endDate;
-    const years = moment(endDate).diff(job.startDate, 'years');
-    const months = moment(endDate).diff(job.startDate, 'months') % 12;
-    const startDateText = moment(job.startDate).format('MMM YYYY');
+    const years = dayjs(endDate).diff(job.startDate, 'years');
+    const months = dayjs(endDate).diff(job.startDate, 'months') % 12;
+    const startDateText = dayjs(job.startDate).format('MMM YYYY');
     const endDateText =
-      job.endDate === 'Present' ? 'Present' : moment(job.endDate).format('MMM YYYY');
+      job.endDate === 'Present' ? 'Present' : dayjs(job.endDate).format('MMM YYYY');
     return `${startDateText} — ${endDateText} ${years} years ${months} months`;
   });
 }
